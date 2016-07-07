349bd2efbfa7c4465d2e6a88c78770ac61812342
Benjam Welker <benjamwelker@gmail.com>
2016-07-07 02:35:57 -0600

    Initial file commit

---
+++ assets/js/spies.js
@@ -0,0 +1,534 @@
+/**
+ * Resistance: A Deduction Game (renamed to Spies because shorter)
+ */
+;
+function Spies( ) {
+    "use strict";
+
+    this.players = [];
+    this.team = [];
+    this.votes = [];
+    this.last_votes = [];
+    this.current_leader = null;
+    this.current_mission = -1;
+    this.current_player = null;
+    this.mission_running = false;
+    this.last_mission_result = false;
+    this.failed_votes = 0;
+    this.missions = [];
+    this.failed_missions = 0;
+    this.succeeded_missions = 0;
+    this.winners = false;
+
+    this.teams = {
+        5: [3, 2],
+        6: [4, 2],
+        7: [4, 3],
+        8: [5, 3],
+        9: [6, 3],
+        10: [6, 4]
+    };
+
+    this.team_size = {
+        5: [2, 3, 2, 3, 3],
+        6: [2, 3, 3, 3, 4],
+        7: [2, 3, 3, 4, 4],
+        8: [3, 4, 4, 5, 5],
+        9: [3, 4, 4, 5, 5],
+        10: [3, 4, 4, 5, 5]
+    };
+
+    this.required_fails = {
+        5: [1, 1, 1, 1, 1],
+        6: [1, 1, 1, 1, 1],
+        7: [1, 1, 1, 2, 1],
+        8: [1, 1, 1, 2, 1],
+        9: [1, 1, 1, 2, 1],
+        10: [1, 1, 1, 2, 1]
+    };
+}
+
+
+/**
+ * Set data from outside
+ *
+ * @param data
+ */
+Spies.prototype.setData = function(data) {
+    for (var elem in data) {
+        if (data.hasOwnProperty(elem) && this.hasOwnProperty(elem)) {
+            this[elem] = data[elem];
+        }
+    }
+};
+
+
+/**
+ * Reset the game but keep the current players
+ */
+Spies.prototype.newGame = function () {
+    "use strict";
+
+    this.team = [];
+    this.votes = [];
+    this.last_votes = [];
+    this.current_leader = null;
+    this.current_mission = -1;
+    this.current_player = null;
+    this.mission_running = false;
+    this.last_mission_result = false;
+    this.failed_votes = 0;
+    this.missions = [];
+    this.failed_missions = 0;
+    this.succeeded_missions = 0;
+    this.winners = false;
+
+    for (var i = 0; i < this.players.length; i += 1) {
+        this.players[i].spy = false;
+    }
+};
+
+
+/**
+ * Add a player to the player list
+ *
+ * @param name string
+ * @returns boolean name added successfully
+ */
+Spies.prototype.addPlayer = function(name) {
+    "use strict";
+
+    // test for player with the same name
+    name = name.trim( );
+
+    for (var i = 0; i < this.players.length; i += 1) {
+        if (name === this.players[i].name) {
+            return false;
+        }
+    }
+
+    var player = {
+        'id': this.players.length,
+        'name': name,
+        'spy': false
+    };
+
+    this.players.push(player);
+
+    return true;
+};
+
+
+/**
+ * Remove a player from the player list
+ *
+ * @param id
+ */
+Spies.prototype.removePlayer = function(id) {
+    "use strict";
+
+    this.players.splice(id, 1);
+};
+
+
+
+/**
+ * Done adding players. Start the missions
+ */
+Spies.prototype.doneAddingPlayers = function( ) {
+    "use strict";
+
+    this.generateSpies( );
+    this.nextMission( );
+    this.nextPlayer( );
+};
+
+
+/**
+ * Randomly select the spies for the game
+ */
+Spies.prototype.generateSpies = function( ) {
+    "use strict";
+
+    // pull out the required number of random players to be spies
+    var current;
+    var names = shuffle(this.players);
+
+    for (var i = 0; i < this.teams[this.players.length][1]; i += 1) {
+        current = names[i].id;
+        this.players[current].spy = true;
+    }
+};
+
+
+/**
+ * Is the given player a spy
+ *
+ * @param id
+ * @returns boolean player is a spy
+ */
+Spies.prototype.isSpy = function(id) {
+    "use strict";
+
+    return this.players[id].spy;
+};
+
+
+/**
+ * Selects the next mission leader
+ */
+Spies.prototype.nextLeader = function( ) {
+    "use strict";
+
+    // randomize first leader
+    if (null === this.current_leader) {
+        // because this gets incremented below, decrease all valid values by 1
+        this.current_leader = rand(-1, this.players.length - 2);
+    }
+
+    this.current_leader += 1;
+
+    if (this.current_leader >= this.players.length) {
+        this.current_leader = 0;
+    }
+};
+
+
+/**
+ * Starts the next mission
+ * Tests for game completion
+ */
+Spies.prototype.nextMission = function( ) {
+    "use strict";
+
+    this.last_votes = this.votes;
+
+    this.nextLeader( );
+
+    if (3 <= this.succeeded_missions) {
+        this.setWinners('Resistance');
+        return;
+    }
+    else if (3 <= this.failed_missions) {
+        this.setWinners('Spies');
+        return;
+    }
+
+    this.current_mission += 1;
+    this.mission_running = false;
+
+    this.team = [];
+    this.votes = [];
+};
+
+
+/**
+ * Sets the selected team for the next mission
+ *
+ * @param team
+ * @returns boolean team was successfully set
+ */
+Spies.prototype.setTeam = function(team) {
+    if (this.team_size[this.players.length][this.current_mission] !== team.length) {
+        return false;
+    }
+
+    this.team = team;
+    return true;
+};
+
+
+/**
+ * submit a team vote
+ * If `id` is boolean, the entire vote is passed or failed as given
+ *
+ * @param id
+ * @param vote
+ * @returns {boolean}
+ */
+Spies.prototype.teamVote = function(id, vote) {
+    "use strict";
+
+    if (false === id) {
+        this.failTeamVote( );
+    }
+    else if (true === id) {
+        this.startMission( );
+    }
+    else {
+        if ('undefined' !== typeof this.team_votes[id]) {
+            return false;
+        }
+
+        this.team_votes[id] = !! vote;
+
+        if (this.team_votes.length === this.players.length) {
+            this.tallyTeamVotes( );
+        }
+    }
+
+    return true;
+};
+
+
+/**
+ * Marks this team vote as failed
+ * If 5 team votes fail, the Spies win
+ */
+Spies.prototype.failTeamVote = function( ) {
+    "use strict";
+
+    this.failed_votes += 1;
+
+    if (5 <= this.failed_votes) {
+        this.setWinners('Spies');
+        return;
+    }
+
+    this.nextLeader( );
+};
+
+
+/**
+ * Count up the team votes
+ */
+Spies.prototype.tallyTeamVotes = function( ) {
+    "use strict";
+
+    var votes_for = 0;
+    var votes_against = 0;
+
+    for (var id in this.team_votes) {
+        if (this.team_votes.hasOwnProperty(id)) {
+            if (this.team_votes[id]) {
+                votes_for += 1;
+            }
+            else {
+                votes_against += 1;
+            }
+        }
+    }
+
+    if (votes_for > votes_against) {
+        this.startMission( );
+    }
+    else {
+        this.failTeamVote( );
+    }
+};
+
+
+/**
+ * Start the current mission
+ */
+Spies.prototype.startMission = function( ) {
+    "use strict";
+
+    this.mission_running = true;
+    this.last_mission_result = false;
+    this.current_player = null;
+    this.nextPlayer( );
+};
+
+
+/**
+ * Go to the next player in the mission
+ *
+ * @returns {boolean|int} next player or false if no more left in team
+ */
+Spies.prototype.nextPlayer = function( ) {
+    "use strict";
+
+    var team = this.team;
+
+    if ( ! team.length) {
+        // this is fancy JavaScript for range(0, n) -> [0, 1, 2, 3, ..., n]
+        team = Array.apply(null, Array(this.players.length)).map(function (_, i) { return i; });
+    }
+
+    if (null === this.current_player) {
+        this.current_player = team[0];
+        return this.current_player;
+    }
+
+    var idx = team.indexOf(this.current_player);
+    idx += 1;
+
+    if (idx >= team.length) {
+        this.current_player = null;
+        return false;
+    }
+
+    this.current_player = team[idx];
+    return this.current_player;
+};
+
+
+/**
+ * Submit a mission vote
+ *
+ * @param vote
+ * @returns {boolean}
+ */
+Spies.prototype.missionVote = function(vote) {
+    "use strict";
+
+    // if player[id] is not a spy, make sure vote is 'success'
+    if ( ! this.isSpy(this.current_player)) {
+        vote = true;
+    }
+
+    this.votes.push({'id': this.current_player, 'vote': vote});
+
+    this.nextPlayer( );
+};
+
+
+/**
+ * Finish the mission and set the results
+ *
+ * @returns {*} mission result (1 | -1) or false on failure
+ */
+Spies.prototype.finishMission = function( ) {
+    "use strict";
+
+    if (this.votes.length !== this.team.length) {
+        this.last_mission_result = false;
+        return false;
+    }
+
+    var fail_votes = 0;
+    var team = [];
+    for (var i = 0; i < this.votes.length; i += 1) {
+        if ( ! this.votes[i].vote) {
+            fail_votes += 1;
+        }
+
+        team.push(this.votes[i].id);
+    }
+
+    // make sure everybody on the team voted
+    if (this.team.diff(team).length) {
+        this.last_mission_result = false;
+        return false;
+    }
+
+    if (fail_votes >= this.required_fails[this.players.length][this.current_mission]) {
+        this.last_mission_result = -1;
+        this.failed_missions += 1;
+    }
+    else {
+        this.last_mission_result = 1;
+        this.succeeded_missions += 1;
+    }
+
+    this.nextMission( );
+
+    return this.last_mission_result;
+};
+
+
+/**
+ * Update the mission markers
+ */
+Spies.prototype.updateMissionMarkers = function( ) {
+    "use strict";
+
+    if (1 === this.last_mission_result) {
+        this.missions.push(true);
+    }
+    else if (-1 === this.last_mission_result) {
+        this.missions.push(false);
+    }
+};
+
+
+/**
+ * Set the game winners (Resistance | Spies)
+ *
+ * @param team string
+ */
+Spies.prototype.setWinners = function(team) {
+    "use strict";
+
+    this.winners = team;
+};
+
+
+/**
+ * Get the names of the current mission team
+ *
+ * @returns {Array}
+ */
+Spies.prototype.getTeam = function( ) {
+    "use strict";
+
+    var team = [];
+    for (var i = 0; i < this.team.length; i += 1) {
+        team.push(this.players[this.team[i]].name);
+    }
+
+    return team;
+};
+
+
+/// FUNCTIONS
+
+/**
+ * shuffle an array clone
+ *
+ * @param array array
+ * @return array shuffled array clone
+ */
+function shuffle(array) {
+    "use strict";
+
+    // make sure this is a clone, not the original array
+    array = array.slice(0);
+
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+/**
+ * randomly pick a value between two values, inclusive
+ *
+ * @param min int
+ * @param max int
+ * @return int
+ */
+function rand(min, max) {
+    "use strict";
+
+    return parseInt(Math.floor(Math.random( ) * (max - min + 1)) + min, 10);
+}
+
+
+if ( ! Array.prototype.diff) {
+    /**
+     * Diff an array with another array and return the
+     * elements in the first array that are not in the
+     * second array
+     *
+     * @param arr
+     * @returns Array
+     */
+    Array.prototype.diff = function(arr) {
+        return this.filter( function(elem) { return (arr.indexOf(elem) < 0); } );
+    }
+}
---
+++ assets/js/spies_controller.js
@@ -0,0 +1,361 @@
+
+;
+(function($, Spies) {
+    "use strict";
+
+    var Spy = new Spies;
+
+    // events
+
+    $('#main')
+        // player name entry form
+        .on('submit', '#players_entry_form', function(evt) {
+            evt.preventDefault( );
+
+            if ( ! Spy.addPlayer($('#name').val( ))) {
+                alert('A player with that name already exists');
+                display_players( );
+                return;
+            }
+
+            $('#name').val('');
+
+            display_players( );
+        })
+        // remove player icon
+        .on('click', 'span.remove', function(evt) {
+            var id = this.id.substr(7);
+
+            Spy.removePlayer(id);
+
+            display_players( );
+        })
+        // start game button
+        .on('click', '#start_game:not(.disabled)', function(evt) {
+            Spy.doneAddingPlayers( );
+            render('view_alliance_pass', {'name': Spy.players[Spy.current_player].name});
+        })
+        // show alliance button
+        .on('click', '#show_alliance', function(evt) {
+            var name = Spy.players[Spy.current_player].name;
+            if (confirm('Are you sure you are ' + name + '?')) {
+                render('show_alliance', {'spy': Spy.players[Spy.current_player].spy});
+            }
+        })
+        // alliance done button
+        .on('click', '#alliance_done', function(evt) {
+            var player = Spy.nextPlayer( );
+
+            if (false === player) {
+                render('script');
+            }
+            else {
+                render('view_alliance_pass', {'name': Spy.players[Spy.current_player].name});
+            }
+        })
+        // script done button
+        .on('click', '#script_done', function(evt) {
+            render('view_leader_pass', {'name': Spy.players[Spy.current_leader].name});
+        })
+        // create team button
+        .on('click', '#create_team', function(evt) {
+            render('create_team', {'count': Spy.team_size[Spy.players.length][Spy.current_mission], 'players': Spy.players});
+        })
+        // team player
+        .on('click', '.team_player', function(evt) {
+            if ($(this).prop('checked')) {
+                var count = parseInt($('span.team_count').text( ), 10) - 1;
+                $('span.team_count').text(count);
+                if (0 === count) {
+                    $('input.button')
+                        .removeClass('disabled')
+                        .prop('disabled', false);
+                    $('input[type="checkbox"]')
+                        .not(':checked')
+                        .addClass('disabled')
+                        .prop('disabled', true);
+                }
+            }
+            else {
+                var count = parseInt($('span.team_count').text( ), 10) + 1;
+                $('span.team_count').text(count);
+                if (0 < count) {
+                    $('input.button')
+                        .addClass('disabled')
+                        .prop('disabled', true);
+                    $('input[type="checkbox"]')
+                        .removeClass('disabled')
+                        .prop('disabled', false);
+                }
+            }
+        })
+        // done creating team
+        .on('submit', '#create_team_form', function(evt) {
+            evt.preventDefault( );
+
+            var team = [];
+
+            $('input[type="checkbox"]:checked').each( function(i, elem) {
+               team.push(elem.name.substr(7));
+            });
+
+            Spy.setTeam(team);
+
+            render('team_vote', {'team': Spy.getTeam( )});
+        })
+        // mission vote passed
+        .on('click', '#mission_vote_accept', function(evt) {
+            Spy.teamVote(true);
+            render('do_mission_pass', {'name': Spy.players[Spy.current_player].name});
+        })
+        // mission vote failed
+        .on('click', '#mission_vote_reject', function(evt) {
+            Spy.teamVote(false);
+            render('view_leader_pass', {'name': Spy.players[Spy.current_leader].name});
+        })
+        // mission button
+        .on('click', '#do_mission', function(evt) {
+            var name = Spy.players[Spy.current_player].name;
+            if (confirm('Are you sure you are ' + name + '?')) {
+                render('mission_vote', {'pre': Array(rand(0, 3)).fill(1), 'alt': (rand(0, 1) ? true : false), 'post': Array(rand(0, 3)).fill(1)});
+            }
+        })
+        // mission success
+        .on('click', '#mission_success', function(evt) {
+            Spy.missionVote(true);
+            var result = Spy.finishMission( );
+
+            if (false !== result) {
+                render('mission_done', {}, true);
+            }
+            else {
+                render('do_mission_pass', {'name': Spy.players[Spy.current_player].name});
+            }
+        })
+        // mission fail
+        .on('click', '#mission_fail', function(evt) {
+            Spy.missionVote(false);
+            var result = Spy.finishMission( );
+
+            if (false !== result) {
+                render('mission_done', {}, true);
+            }
+            else {
+                render('do_mission_pass', {'name': Spy.players[Spy.current_player].name});
+            }
+        })
+        // mission done button
+        .on('click', '#mission_done', function(evt) {
+            var result;
+            var successes = 0;
+            var fails = 0;
+
+            if (1 === Spy.last_mission_result) {
+                result = true;
+            }
+            else if (-1 === Spy.last_mission_result) {
+                result = false;
+            }
+            else {
+                render('error', {'msg': 'The mission has not completed yet'});
+            }
+
+            for (var i = 0; i < Spy.last_votes.length; i += 1) {
+                if (Spy.last_votes[i].vote) {
+                    successes += 1;
+                }
+                else {
+                    fails += 1;
+                }
+            }
+
+            Spy.updateMissionMarkers( );
+
+            render('show_results', {'result': result, 'successes': successes, 'fails': fails}, true);
+        })
+        // results done button
+        .on('click', '#results_done', function(evt) {
+            render('view_leader_pass', {'name': Spy.players[Spy.current_leader].name});
+        })
+        ;
+    
+
+    // functions
+
+    function check_game_over( ) {
+        if (Spy.winners) {
+            var resistance = ('Resistance' === Spy.winners);
+            render('game_over', {'resistance': resistance}, true);
+            throw 'Game Over'; // exit all remaining script
+        }
+    }
+
+    function display_players( ) {
+        var player_count = 5;
+        var $start_button = $('#start_game');
+
+        $('#players').empty( );
+        $.each(Spy.players, function (i, elem) {
+            $('#players').append('<li><span class="icon fa-times spy remove" id="remove_' + i + '"></span> &nbsp; ' + elem.name + '</li>');
+        });
+
+        player_count = player_count - Spy.players.length;
+
+        if (0 > player_count) {
+            player_count = 0;
+        }
+
+        $start_button
+            .text('Start Game')
+            .removeClass('disabled')
+            .prop('disabled', false);
+
+        if (player_count) {
+            $start_button
+                .text(player_count + ' more players')
+                .addClass('disabled')
+                .prop('disabled', true);
+        }
+
+        $('#players_entry_form').show( );
+        if (10 <= Spy.players.length) {
+            $('#players_entry_form').hide( );
+        }
+
+        store_data( );
+    }
+
+
+    function update_markers( ) {
+        var $missions = $('#mission_markers').find('span');
+        var $votes = $('#failed_votes_markers').find('span');
+
+        // reset all the markers
+        $missions.attr('class', 'icon fa-circle-o');
+        $votes.attr('class', 'icon fa-circle-o');
+
+        $missions.each( function(i, elem) {
+           if ('undefined' !== typeof Spy.missions[i]) {
+               if (Spy.missions[i]) {
+                   $(elem).attr('class', 'icon fa-circle resistance');
+               }
+               else {
+                   $(elem).attr('class', 'icon fa-circle spy');
+               }
+           }
+        });
+
+        $votes.each( function(i, elem) {
+           if (i < Spy.failed_votes) {
+               $(elem).attr('class', 'icon fa-times-circle spy');
+           }
+        });
+    }
+
+
+    function render(template, data, skip_check_game_over) {
+        skip_check_game_over = !! skip_check_game_over;
+
+        if ( ! skip_check_game_over) {
+            check_game_over( );
+        }
+
+        $('#main').empty( ).append(Templates[template].render(data));
+
+        store_data( );
+        update_markers( );
+    }
+
+
+    function store_data( ) {
+        console.log('Storing data: ');
+        console.log(Spy);
+        console.log(JSON.stringify(Spy));
+        sessionStorage.setItem('Spies', JSON.stringify(Spy));
+    }
+
+
+    function get_data( ) {
+        var data = sessionStorage.getItem('Spies');
+
+        if (data) {
+            data = JSON.parse(data);
+        }
+
+        return data;
+    }
+
+
+    function clear_data( ) {
+        sessionStorage.setItem('Spies', '');
+    }
+
+
+    // instantiation
+
+    var data = get_data();
+    if (!data || ('undefined' === typeof data.players)) {
+        render('intro');
+    }
+    else {
+        Spy = new Spies;
+        Spy.setData(data);
+    }
+
+    switch (window.location.hash) {
+        case '#reset' :
+            Spy = new Spies;
+            clear_data( );
+            window.location = window.location.href.split('#')[0];
+            break;
+
+        case '#new' :
+            Spy.newGame( );
+            store_data( );
+            window.location = window.location.href.split('#')[0];
+            break;
+
+        case '#undo' :
+            // not sure yet...
+            window.location = window.location.href.split('#')[0];
+            break;
+
+        default :
+            // do nothing
+            break;
+    }
+
+    // refresh on menu item clicks
+    $('a[href^="#"]').not('a[href="#nav"]').on('click', function(evt) {
+        store_data( );
+        window.location.reload( );
+    });
+
+
+    // refreshiation
+    // at the bottom because it may forcefully quit on render
+
+    if (null === Spy.current_leader) {
+        render('intro');
+        display_players( );
+    }
+    else if ( ! Spy.mission_running && (null !== Spy.current_player)) {
+        render('view_alliance_pass', {'name': Spy.players[Spy.current_player].name});
+    }
+    else if (-1 !== Spy.current_mission) {
+        render('view_leader_pass', {'name': Spy.players[Spy.current_leader].name});
+    }
+    else if (false === Spy.finishMission( )) {
+        render('do_mission_pass', {'name': Spy.players[Spy.current_player].name});
+    }
+    else if ( ! Spy.mission_running) {
+        render('team_vote', {'team': Spy.getTeam( )});
+    }
+    else if (Spy.mission_running) {
+        if (null === Spy.current_player) {
+            Spy.nextPlayer( );
+        }
+        render('do_mission_pass', {'name': Spy.players[Spy.current_player].name});
+    }
+
+})(jQuery, Spies);
